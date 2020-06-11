ca62f8f36e297bd7633aaadf5034e7bce4d054d1
Jackson Strauss <58577918+Jastr0445@users.noreply.github.com>
2020-06-11 00:33:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ const axios = require("axios");
 
 const writeFileAsync = util.promisify(fs.writeFile);{
 
-  
+var questions = [];
+
+
+function init() {
+
 }
+
+init();
+
  inquirer.prompt([
     {
       type: "input",
@@ -25,32 +32,28 @@ const writeFileAsync = util.promisify(fs.writeFile);{
     },
 
    
-  ]);
-}
-   
-// All of the `createGreeting` functions are equivalent
-var createGreeting = function(message, name) {
-  return message + ", " + name + "!";
+  ]) .then(function({ username }) {
+    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+
+    axios.get(queryUrl).then(function(res) {
+      const repoNames = res.data.map(function(repo) {
+        return repo.name;
+      });
+
+      const repoNamesStr = repoNames.join("\n");
+
+      fs.writeFile("repos.txt", repoNamesStr, function(err) {
+        if (err) {
+          throw err;
+        }
+
+        console.log(`Saved ${repoNames.length} repos`);
+      });
+    });
+  });
+
 };
-
-// We can safely swap out function expressions with arrow functions most of the time
-var createGreeting = (message, name) => {
-  return message + ", " + name + "!";
-};
-
-// If the arrow function body contains only one expression, we can omit the curly braces and auto return it
-var createGreeting = (message, name) => message + ", " + name + "!";
-
-// If an arrow function only has one parameter, we can omit the parens () around the single parameter
-var greet = greeting => console.log(greeting);
-
-// We call arrow functions the same way as we call regular functions
-var greeting = createGreeting("Hello", "user");
-
-// Logs "Hello, Angie!";
-greet(greeting);
-
-
+  
 
  promptUser()
    .then(function(answers) {
